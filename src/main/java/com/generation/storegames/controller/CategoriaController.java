e8c8f92bdd44d845af99bb7dd56b3a07e22f3c4c
Hericlis Ventura <hericlisventura.o@gmail.com>
2022-07-19 12:31:55 -0300

    adicionando metodo de buscar pro id na categoria

--- src/main/java/com/generation/storegames/controller/CategoriaController.java
+++ src/main/java/com/generation/storegames/controller/CategoriaController.java
@@ -1,5 +1,36 @@
 package com.generation.storegames.controller;
 
-public class CategoriaController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.storegames.model.Categoria;
+import com.generation.storegames.repository.CategoriaRepository;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/categoria")
+public class CategoriaController {
+	
+	
+	@Autowired
+	private CategoriaRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> getAll(){
+		return ResponseEntity.ok(repository.findAll());
+	}
+	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> getById(@PathVariable Long id){
+		return repository.findById(id).map(categoria -> ResponseEntity.ok(categoria))
+				.orElse(ResponseEntity.notFound().build());
+	}
 }
