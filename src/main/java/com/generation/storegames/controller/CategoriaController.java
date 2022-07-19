849489af8e55201ad64ceb576a3ef94dcf58d225
Hericlis Ventura <hericlisventura.o@gmail.com>
2022-07-19 12:37:38 -0300

    adicionando endpoint de delete de categoria

--- src/main/java/com/generation/storegames/controller/CategoriaController.java
+++ src/main/java/com/generation/storegames/controller/CategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -51,5 +52,11 @@ public class CategoriaController {
 	@PutMapping
 	public ResponseEntity<Categoria> editarCategoria(@RequestBody Categoria categoria){
 		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void deletarCategoria(@PathVariable long id) {
+		repository.deleteById(id);
+	
 	}
 }
