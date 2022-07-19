a8f9c3de8f44c365c8fb7113a4cc9b4467bd275d
Hericlis Ventura <hericlisventura.o@gmail.com>
2022-07-19 12:21:43 -0300

    Adicionando uma busca especifica pelo nome do produto

--- src/main/java/com/generation/storegames/controller/ProdutoController.java
+++ src/main/java/com/generation/storegames/controller/ProdutoController.java
@@ -6,11 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.generation.storegames.model.Categoria;
-import com.generation.storegames.repository.CategoriaRepository;
+import com.generation.storegames.model.Produto;
+import com.generation.storegames.repository.ProdutoRepository;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -18,10 +19,23 @@ import com.generation.storegames.repository.CategoriaRepository;
 public class ProdutoController {
 
 	@Autowired
-	private CategoriaRepository repository;
+	private ProdutoRepository repository;
 	
 	@GetMapping
-	public ResponseEntity<List<Categoria>> getAll(){
+	public ResponseEntity<List<Produto>> getAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> getById(@PathVariable Long id){
+		return repository.findById(id).map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	
+	
+	@GetMapping("/nome/{nome}")
+	public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome){
+		return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+	}
+	
 }
