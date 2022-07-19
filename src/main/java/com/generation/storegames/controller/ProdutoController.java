9c1d1b13a013f7bd7f5b7a05087ea855334bc594
Hericlis Ventura <hericlisventura.o@gmail.com>
2022-07-19 12:02:15 -0300

    configurando o aplicativo .properties e criando as classes de Controller

--- src/main/java/com/generation/storegames/controller/ProdutoController.java
+++ src/main/java/com/generation/storegames/controller/ProdutoController.java
@@ -1,5 +1,12 @@
 package com.generation.storegames.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RequestMapping("/produto")
 public class ProdutoController {
 
 }
